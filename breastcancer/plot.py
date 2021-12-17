fbf6562bb5a4bf36b3d44911b5366c91217c9ea0
kristoforusbryant <kristoforusbryant@gmail.com>
2021-12-17 14:46:00 +0800

    change color map to grayscale

--- breastcancer/plot.py
+++ breastcancer/plot.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
-import sys, pickle
-import numpy as np 
-
-from cbmcmc import Uniform, StarCycleBases, OneEdge, MCMC_Sampler_Ratio, GW_Ratio
-from utils.myGraph import Graph
-from utils.diagnostics import IAT, str_list_to_adjm
+import numpy as np
+from utils.diagnostics import IAT
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def main():
     n, n_obs = 93, 250
     gname = 'breastcancer'
     data = np.loadtxt(f"data/{gname}_data_{n}_{n_obs}.csv", delimiter=",")
 
+    print('Loading results...')
     # Edges
     edge_sizes = np.loadtxt(f"res/edge_sizes_{gname}_{n}_{n_obs}.csv", delimiter=',')
     edge_adjm = np.loadtxt(f"res/edge_adjm_{gname}_{n}_{n_obs}.csv", delimiter=',')
@@ -20,22 +19,22 @@ def main():
     triangles_sizes = np.loadtxt(f"res/triangles_sizes_{gname}_{n}_{n_obs}.csv", delimiter=',')
     triangles_adjm = np.loadtxt(f"res/triangles_adjm_{gname}_{n}_{n_obs}.csv", delimiter=',')
 
-    # Plotting Trace Plot
-    import matplotlib.pyplot as plt
-    plt.plot(edge_sizes, label='Edges')
-    plt.plot(triangles_sizes, label='Star Cycle Bases')
 
-    plt.xlabel('Iterations', fontsize=15)
-    plt.ylabel('Size', fontsize=15)
-    plt.legend(loc='upper right', fontsize=12)
+    print('Plotting Trace Plots...')
+    # plt.plot(edge_sizes, label='Edges')
+    # plt.plot(triangles_sizes, label='Star Cycle Bases')
 
-    y_pos = min(edge_sizes[1000:])
-    txt = f"IAT: {round(IAT(edge_sizes), 3)} (Edges),\n"
-    txt += f"IAT: {round(IAT(triangles_sizes), 3)} (Star Cycle Bases)"
-    plt.text(1, y_pos, txt, fontsize=14)
+    # plt.xlabel('Iterations', fontsize=15)
+    # plt.ylabel('Size', fontsize=15)
+    # plt.legend(loc='upper right', fontsize=12)
 
-    plt.title('Graph Sizes Trace Plot', fontsize=16)
-    plt.savefig(f'figs/compare_{gname}_{n}_{n_obs}_traces.pdf', bbox_inches='tight')
+    # y_pos = min(edge_sizes[1000:])
+    # txt = f"IAT: {round(IAT(edge_sizes), 3)} (Edges),\n"
+    # txt += f"IAT: {round(IAT(triangles_sizes), 3)} (Star Cycle Bases)"
+    # plt.text(1, y_pos, txt, fontsize=14)
+
+    # plt.title('Graph Sizes Trace Plot', fontsize=16)
+    # plt.savefig(f'figs/compare_traces_{gname}_{n}_{n_obs}.pdf', bbox_inches='tight')
 
     # Plotting Comparisons with glasso, BDgraph, and GeneNet
     from sklearn.covariance import GraphicalLassoCV
@@ -57,27 +56,29 @@ def main():
         edgeM[tril] = edgeM[tril] > .5
         return edgeM
 
-    fig, axs = plt.subplots(2, 4, figsize=(20, 10))
+    print('Plotting Heatmaps...')
+    fig, axs = plt.subplots(2, 4, figsize=(40, 20))
 
-    axs[0, 0].imshow(get_adjm_split(edge_adjm))
-    axs[0, 0].set_title("Edges", fontsize=20)
-    axs[0, 1].imshow(get_adjm_split(triangles_adjm))
-    axs[0, 1].set_title("Star Cycle Bases", fontsize=20)
-    axs[0, 2].imshow(get_adjm_split(bd_adjm))
-    axs[0, 2].set_title("BDgraph", fontsize=20)
-    axs[0, 3].imshow(glasso_adjm)
-    axs[0, 3].set_title("Glasso", fontsize=20)
-    axs[1, 0].imshow(get_adjm_split(gnet_adjm1))
-    axs[1, 0].set_title("GeneNet (pval < 0.001)", fontsize=20)
-    axs[1, 1].imshow(get_adjm_split(gnet_adjm2))
-    axs[1, 1].set_title("GeneNet (pval < 0.01)", fontsize=20)
-    axs[1, 2].imshow(get_adjm_split(gnet_adjm3))
-    axs[1, 2].set_title("GeneNet (pval < 0.05)", fontsize=20)
-    axs[1, 3].imshow(get_adjm_split(gnet_adjm4))
-    axs[1, 3].set_title("GeneNet (pval < 0.1)", fontsize=20)
+    titles = ["Edge", "Star Cycle Bases", "BDgraph", "Graphical Lasso", "GeneNet (pval < 0.001)", "GeneNet (pval < 0.01)", "GeneNet (pval < 0.05)", "GeneNet (pval < 0.1)"]
+    adjms = [get_adjm_split(edge_adjm), get_adjm_split(triangles_adjm), get_adjm_split(bd_adjm), glasso_adjm,\
+                 get_adjm_split(gnet_adjm1), get_adjm_split(gnet_adjm2), get_adjm_split(gnet_adjm3), get_adjm_split(gnet_adjm4)]
 
-    fig.suptitle('Comparing Posterior Inference against other Covariance Selection Algorithms', fontsize=30)
-    fig.savefig(f'figs/comparison_{gname}_{n}_{n_obs}.pdf', bbox_inches='tight')
+    for i in range(2):
+        for j in range(4):
+            idx = i * 4 + j
+            if i == 0:
+                divider = make_axes_locatable(axs[i, j])
+                cax = divider.append_axes('right', size='5%', pad=0.05)
+                im = axs[i, j].imshow(adjms[idx], cmap='Greys')
+                cbar = fig.colorbar(im, cax=cax, orientation='vertical')
+                cbar.ax.tick_params(labelsize=18)
+            else:
+                axs[i, j].imshow(adjms[idx], cmap='Greys')
+            axs[i, j].set_title(titles[idx], fontsize=42)
+            axs[i, j].tick_params(axis='both', which='major', labelsize=22)
+
+    fig.suptitle('Comparing against other Covariance Selection Algorithms', fontsize=55)
+    fig.savefig(f'figs/compare_posterior_{gname}_{n}_{n_obs}.pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
     main()
